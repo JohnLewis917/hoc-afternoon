349a46fa6df5b7a3e947feefdd96db105136f32f
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-13 16:18:46 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CurrencyHOC from './components/CurrencyConverter.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <h2>High order Component</h2>
+        <CurrencyHOC/>
+      </>
     );
   }
 }
